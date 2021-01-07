4fd2b1a0fe1c931fb39af7627aae79ec5a088f12
NoeCampos22 <ama-noe@outlook.com>
2021-01-06 19:00:30 -0600

    (Un)Install Options

--- LaunchURLs.sh
+++ LaunchURLs.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 # Flags to know if the options where or not received
-fileFlg=
-urlFlg=
-webPageFLG=
+installFlg=
+installDependencies=
 
-# Global variables
-fileVal=
-urlVal=
-webPageVal=
+# Global variables that store option values
+alternativeInstPath=
+
+# Needed extra global variables
 
 # Execute getopt on the arguments passed to this program
-PARSED_OPTIONS="$(getopt --n "Testing_GETOPT" -o f:u:p:h -l file:,url:,url=:,pagename:,help -- "$@")"
+PARSED_OPTIONS="$(getopt --n "LaunchURLs" -o ynYNp:h -l install,uninstall,path:,help -- "$@")"
 
 # If any bad argument was received
 [ $? -eq 0 ] || { 
-    echo "Incorrect options provided"
+    echo -e "\nInvalid options provided\n"
     exit 1
 }
 
@@ -23,26 +22,128 @@ PARSED_OPTIONS="$(getopt --n "Testing_GETOPT" -o f:u:p:h -l file:,url:,url=:,pag
 eval set -- "$PARSED_OPTIONS"
 unset PARSED_OPTIONS
 
-# Goes through all the options with a case and shifting when needing
-# If it is just the option, it needs to shift 1 but if there is also a
-# expected value, it needs to shift 2 and so on.
+# Function that install the script on the specified path
+# Or by default it will be moved to the /usr/bin/ directory
+installScript() {
+
+	# Notify about the dependency and ask for permission
+	echo 'This program has a dependcy on the package: wmctrl'
+
+	# Check if wmctrl is already installed or not
+	if ! command -v wmctrl >/dev/null 2>&1 ; then
+
+		# If the install dependencies option was not received...
+		if [ ! -n "$installDependencies" ]; then
+
+			# Ask the user
+			while true; do
+			read -p "Do you want to install: wmctrl? (Y/n) " installDependencies
+				case $installDependencies in
+					[YyNn]* )
+						break;;
+
+					* ) 
+						echo -e "\nPlease answer yes (y/Y) or no (n/N).\n";
+				esac
+			done
+		fi
+
+		# Install or not the dependency
+		case $installDependencies in
+			1) 
+				echo "";
+				sudo apt-get install wmctrl -y; 
+				echo "";;
+
+			0)
+				echo -e "\nCan not install LaunchURL without wmctrl\n";
+				exit;;
+		esac
+		
+	else
+		echo -e "wmctrl is already installed\n";
+	fi
+
+	# Build the current and target location
+	CURR_LOC="$(pwd)/LaunchURLs.sh";
+
+	# Build the installation path
+	if [ -n "$alternativeInstPath" ]; then
+		TARG_LOC="$alternativeInstPath/LaunchURLs";
+	else
+		TARG_LOC="/usr/bin/LaunchURLs";
+	fi
+
+	# Move the file to the target location
+	sudo cp $CURR_LOC $TARG_LOC
+
+	# Create the directory for the .desktop file
+	mkdir ~/.local/share/applications/LaunchURLs_DeskFiles
+
+	echo -e "\nLaunchURLs was succesfully installed!\n"
+}
+
+
+# Execute the solicited option
 while true; do
     case $1 in
 
-    -f|--file)
-        fileFlg=1;
-        fileVal=$2;
-        shift 2;;
-    
-    -u|--url|--url=)
-        urlFlg=1;
-        urlVal=$2;
-        shift 2;;
-    
-    -p|--pagename)
-        webPageFLG=1;
-        webPageVal=$2;
-        shift 2;;
+    --install)
+		shift 1;
+
+		# Check if the user already answer the
+		# dependency permissions question via an option
+		case $1 in
+			-y|-Y)
+				installDependencies=1;
+				shift 1;;
+
+			-n|-N)
+				installDependencies=0;
+				shift 1;;
+		esac
+
+		# Check if the user sent an alternative installation path
+		case $1 in
+			-p|--path)
+
+				# Check that the received path is to a valid directory
+				if [[ ! -d $2 ]]; then
+					echo -e "\nThe path must be to a valid directory.\n"
+					exit 1;
+				fi
+
+				alternativeInstPath=$2;
+				shift 2;;
+		esac
+		
+		if [ "$#" -gt 1 ]; then
+			echo -e "\nAny option different than -y/Y, -n/N or --path after --install will be ignored."
+		fi
+
+		# Call the function that makes the validations
+		# and installation
+		installScript
+		exit 1;
+		;;
+
+	--uninstall)
+		shift 1;
+
+		if [ "$#" -gt 1 ]; then
+			echo -e "\nAny option after --uninstall will be ignored."
+		fi
+
+		# Remove the created .desktop files
+		echo -e "\nThe .desktop files created by LaunchURLs will also be removed."
+		rm -rf ~/.local/share/applications/LaunchURLs_DeskFiles
+
+		# Get the path to the command and delete the file
+		PATH=$(which LaunchURLs)
+		echo -e "LaunchURLs was uninstalled succesfully!\n"
+		exit 1;
+		;;
+
     
     -h|--help)
         echo "Usage (Print the Manual)"
@@ -52,89 +153,9 @@ while true; do
         shift
         break;;
     
-    # Should not occur
     *) 
         echo "Unknown error while processing options";
         exit 1;;
 
     esac
 done
-
-if [[ -z "$urlFlg" || -z "$webPageFLG" || -z "$fileFlg" ]]; then
-    echo "All the parameters are required"
-    exit 1
-fi
-
-
-# Function to open Notion and store the WID
-openURL () {
-
-	# Store the number of previous instances of Notion
-	GREP_RESULT=$(wmctrl -l | grep $webPageVal)
-	PREV=$(echo $GREP_RESULT | awk '{print NF/4}')
-
-	# Calculate the position of the new WID
-	WID_NUM=$((4 * $PREV + 1))
-	echo $WID_NUM
-
-	# Open the Notion web page
-    brave-browser --app=$urlVal &
-
-	# Wait 5 seconds for the page to load
-	sleep 5s
-
-	# And check if the page already load or not
-	GREP_RESULT=$(wmctrl -l | grep $webPageVal) 
-
-	# After that, check each 2 seconds if the page already load
-	while [[ -z "$GREP_RESULT" ]];
-	do 
-		GREP_RESULT=$(wmctrl -l | grep $webPageVal) 
-		sleep 2s; 
-	done
-
-    # Save and append the new WID
-	WID=$(echo $GREP_RESULT | awk -v var=$WID_NUM '{print $(var)}')
-	echo $WID 
-    echo $WID >> $fileVal;
- 
-	#nohup 
-	/home/noecampos/.local/share/applications/LaunchURLS/CloseWindow.sh $WID $fileVal
-}
-
-# Check if the file exists
-if test -f "$fileVal"; then
-
-	# If it is not empty...
-	if [ -s "$fileVal" ]; then
-
-		# Get the WID stored on the file
-		WID=$(cat "$fileVal")
-
-		# Check if WID is currently used with Notion opened as an app
-		GREP_RESULT=$(wmctrl -l | grep $WID)
-		if [ -z "$GREP_RESULT" ]
-		# If the output is empty, it means there is no instance
-		then
-			# Open notion 
-			openURL
-
-		# If grep returns something, it means the app is already opened
-		else			
-			# Bring the window to the front
-			wmctrl -iR $WID
-		fi
-	
-	# Check if the file is empty
-	else
-		# Open notion  
-		openURL
-	fi
-
-# Create it and open Notion
-else
-	# Open notion 
-	openURL
-fi
-
-sleep 10
